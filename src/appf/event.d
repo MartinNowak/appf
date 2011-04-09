7afea0fe4c82fd06f99c14d4d66af69ab9eb7d8c
dawg <dawg@dawgfoto.de>
2011-04-09 13:42:31 +0200

    move events to guip lib

--- src/appf/event.d
+++ src/appf/event.d
@@ -1,7 +1,7 @@
 module appf.event;
 
-import std.algorithm, std.bitmanip, std.conv, std.typetuple, std.variant;
-public import guip.point, guip.rect, guip.size;
+import std.algorithm;
+public import guip.event, guip.point, guip.rect, guip.size;
 
 version (Posix) {
   version = xlib;
@@ -10,67 +10,15 @@ version (Posix) {
   static assert(0);
 }
 
-alias TypeTuple!(StateEvent, MouseEvent, KeyEvent, RedrawEvent, ResizeEvent) EventTypes;
-alias Algebraic!(EventTypes) Event;
-
-void visitEvent(Visitor, Args...)(Event e, Visitor visitor, Args args) {
-  foreach(T; EventTypes) {
-    static if(is(typeof(visitor.visit(e.get!(T), args)))) {
-      if (e.type == typeid(T))
-        visitor.visit(e.get!(T), args);
-    }
-  }
-}
-
-struct StateEvent {
-  bool visible;
-}
-
-struct MouseEvent {
-  IPoint pos;
-  Button button;
-  Mod mod;
-}
-
-struct KeyEvent {
-  IPoint pos;
-  Key key;
-  Mod mod;
-}
-
-struct RedrawEvent {
-  IRect area;
-}
-
-struct ResizeEvent {
-  IRect area;
-}
-
-struct Button {
-  mixin(bitfields!(
-          bool, "left", 1,
-          bool, "middle", 1,
-          bool, "right", 1,
-          uint, "", 5));
-}
-
-struct Mod {
-  mixin(bitfields!(
-          bool, "shift", 1,
-          bool, "ctrl", 1,
-          bool, "alt", 1,
-          bool, "numlock", 1,
-          uint, "", 4));
-}
-
-struct Key {
-  uint num;
-  @property dchar character() const {
-    return cast(dchar)this.num;
-  }
-}
-
 version (xlib) {
+  Button buttonDetail(uint button) {
+    Button btn;
+    btn.left = button == xlib.Button1;
+    btn.middle = button == xlib.Button2;
+    btn.right = button == xlib.Button3;
+    return btn;
+  }
+
   Button buttonState(uint state) {
     Button btn;
     btn.left = (state & xlib.Button1Mask) != 0;
@@ -86,5 +34,42 @@ version (xlib) {
     mod.alt = (state & xlib.Mod1Mask) != 0;
     mod.numlock = (state & xlib.Mod2Mask) != 0;
     return mod;
+  }
+
+  struct ToXEvent {
+    xlib.Display* dpy;
+    xlib.Window hwnd;
+
+    this(xlib.Display* dpy, xlib.Window hwnd) {
+      this.dpy = dpy;
+      this.hwnd = hwnd;
+    }
+
+    xlib.XEvent visit(RedrawEvent e) {
+      xlib.XEvent xe;
+      xe.type = xlib.Expose;
+      xe.xexpose.type = xlib.Expose;
+      xe.xexpose.display = dpy;
+      xe.xexpose.window = hwnd;
+      xe.xexpose.x = e.area.left;
+      xe.xexpose.y = e.area.top;
+      xe.xexpose.width = e.area.width;
+      xe.xexpose.height = e.area.height;
+      return xe;
+    }
+  }
+
+  xlib.XEvent toPlatformEvent(Event e, xlib.Display* dpy, xlib.Window hwnd) {
+    auto conv = ToXEvent(dpy, hwnd);
+    return visitEvent(e, conv);
+  }
+
+  uint xemask(uint type) {
+    switch (type) {
+    case xlib.Expose:
+      return xlib.ExposureMask;
+    default:
+      return xlib.NoEventMask;
+    }
   }
 }
